3f680c18f6f5a98dec16b2df28c2ae2f73b59401
Anton Abilov <anton.abilov@gmail.com>
2022-07-27 23:56:00 +0200

    Updated links

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -9,10 +9,10 @@ const site = {
       name: "Retweet Graph Analysis",
       url: "/retweet-graph",
     },
-    // {
-    //   name: "About",
-    //   url: "/about",
-    // },
+    {
+      name: "Explore Dataset",
+      url: "https://stechlab-voterfraud2020-analysis-app-yrtslm.streamlitapp.com/?page=Explore+The+Dataset",
+    },
     {
       name: "The Paper (arXiv)",
       url: "https://arxiv.org/abs/2101.08210",
